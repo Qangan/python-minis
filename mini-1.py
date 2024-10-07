203affa1822ad7cb7e25a2b969d7c69a13208802
Qangan <90392078+Qangan@users.noreply.github.com>
2024-10-07 20:27:29 +0300

    simpified solve

--- mini-1.py
+++ mini-1.py
@@ -1,15 +1,20 @@
-from math import *
+def actually_count_bits(n: int) -> list:
+    counter, n_len = 0, 0
+    while n > 0:
+        counter += n & 1
+        n >>= 1
+        n_len += 1
+    if counter == n_len:
+        n_len += 1
+    return [counter, n_len]
 def get_ones_from_int(n: int) -> int:
     counter = 0
     if n < 0:
         n = ~n
-        for i in range(31):
-            counter += ((n & (1 << i)) > 0)
-        counter = 1 + (floor(log2(n)) + 1) - counter # один за знаковый бит, длина битового представления [log2 n] + 1
+        result = actually_count_bits(n)
+        return result[1] - result[0] + 1
     else:
-        for i in range(31):
-            counter += ((n & (1 << i)) > 0)
-    return counter
+        result = actually_count_bits(n)
+        return result[0]
 n = int(input("Enter the number: "))
 print(get_ones_from_int(n))
-     
