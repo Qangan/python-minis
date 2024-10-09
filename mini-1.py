756e66d2e8268e448484c07c01dbfc211a7677e2
Daniil Fulman <danilfulmanf@gmail.com>
2024-10-09 11:33:25 +0700

    vrode solved

--- mini-1.py
+++ mini-1.py
@@ -1,4 +1,4 @@
-def actually_count_bits(n: int) -> list:
+def actually_count_bits(n: int) -> tuple:
     counter, n_len = 0, 0
     while n > 0:
         counter += n & 1
@@ -6,15 +6,14 @@ def actually_count_bits(n: int) -> list:
         n_len += 1
     if counter == n_len:
         n_len += 1
-    return [counter, n_len]
+    return counter, n_len
 def get_ones_from_int(n: int) -> int:
-    counter = 0
     if n < 0:
         n = ~n
-        result = actually_count_bits(n)
-        return result[1] - result[0] + 1
+        result, n_len = actually_count_bits(n)
+        return n_len - result + 1
     else:
-        result = actually_count_bits(n)
-        return result[0]
+        result, _ = actually_count_bits(n)
+        return result
 n = int(input("Enter the number: "))
 print(get_ones_from_int(n))
